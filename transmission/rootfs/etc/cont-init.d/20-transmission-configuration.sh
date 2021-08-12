3f8e8b4a48e0f0824733c21e0ba5bfa6549c18f3
mihey86 <mihey19@gmail.com>
2021-08-12 21:31:11 +0300

    Update 20-transmission-configuration.sh

--- transmission/rootfs/etc/cont-init.d/20-transmission-configuration.sh
+++ transmission/rootfs/etc/cont-init.d/20-transmission-configuration.sh
@@ -5,6 +5,9 @@ declare CONFIG
 declare authentication_required
 declare username
 declare password
+declare download_dir
+declare incomplete_dir
+declare incomplete_dir_enabled
 
 if ! bashio::fs.directory_exists '/data/transmission'; then
   mkdir '/data/transmission'
@@ -17,9 +20,9 @@ fi
 CONFIG=$(</data/transmission/settings.json)
 
 # Defaults
-CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir\"=\"/share/incomplete\"")
-CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir-enabled\"=true")
-CONFIG=$(bashio::jq "${CONFIG}" ".\"download-dir\"=\"/share/downloads\"")
+#CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir\"=\"/share/incomplete\"")
+#CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir-enabled\"=true")
+#CONFIG=$(bashio::jq "${CONFIG}" ".\"download-dir\"=\"/share/downloads\"")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-whitelist-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-host-whitelist-enabled\"=false")
 CONFIG=$(bashio::jq "${CONFIG}" ".\"bind-address-ipv4\"=\"0.0.0.0\"")
@@ -27,6 +30,15 @@ CONFIG=$(bashio::jq "${CONFIG}" ".\"bind-address-ipv4\"=\"0.0.0.0\"")
 authentication_required=$(bashio::config 'authentication_required')
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-authentication-required\"=${authentication_required}")
 
+
+download_dir=$(bashio::config 'download_dir')
+CONFIG=$(bashio::jq "${CONFIG}" ".\"download-dir\"=\"${download_dir}\"")
+
+incomplete_dir_enabled=$(bashio::config 'incomplete_dir_enabled')
+CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir-enabled\"=\"${incomplete_dir_enabled}\"")
+
+incomplete_dir_enabled=$(bashio::config 'incomplete_dir')
+CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir\"=\"${incomplete_dir}\"")
 
 username=$(bashio::config 'username')
 CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-username\"=\"${username}\"")
